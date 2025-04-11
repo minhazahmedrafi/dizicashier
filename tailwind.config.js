bb51736c236c95c63f92f4631969341f6fa0bbc3
Minhaz Miah <40710320+minhazahmedrafi@users.noreply.github.com>
2025-04-11 17:07:34 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      mainBlue: "#0a58ca",
+      mainGreen: "#28c54c",
+    },
+  },
+  plugins: [],
+};
